bb33193d3c95c9c4ea73c19e3392147e0a51111c
Jobaid <jobaidazim@gmail.com>
2025-02-15 19:04:44 -0500

    version 1.0.1

---
+++ Live TV/Trends.swift
@@ -0,0 +1,118 @@
+
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+import AlamofireImage
+
+class Trends: UIViewController,UITableViewDataSource, UITableViewDelegate {
+    
+    var ID: String?  // Receiving the ID from previous screen
+    var cat: String?
+    @IBOutlet weak var tblView: UITableView!
+     
+    
+    var refreshControl: UIRefreshControl!
+    var arrRes = [[String: AnyObject]]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Initialize the refresh control
+        tblView.dataSource = self
+        tblView.delegate = self
+        
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refresh(sender:)), for: .valueChanged)
+        tblView.addSubview(refreshControl) // Adding refresh control to table view
+        
+        // Fetch data initially
+        getData()
+    }
+
+    // Table view delegate and data source methods
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 250.0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "jcell")!
+        let dict = arrRes[indexPath.row]
+        
+        if let textLabel = cell.viewWithTag(9) as? UILabel {
+            textLabel.text = dict["n"] as? String
+        }
+        
+        if let imageView = cell.viewWithTag(10) as? UIImageView, let imageURLString = dict["i"] as? String, let url = URL(string: imageURLString) {
+            imageView.af.setImage(withURL: url)
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrRes.count
+    }
+   // Player View
+  //  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    //    if segue.identifier == "id", let indexPath = self.tblView.indexPathForSelectedRow //{
+          //  let controller = segue.destination as! PlayerViewController
+            //let value = arrRes[indexPath.row]
+            //controller.ID = value["link"] as! String
+       // }
+    //}
+
+    // Fetch data from the API
+    func getData() {
+        let apis = ID
+        let cat1 = cat!
+        AF.request(apis!).responseJSON { responseData in
+            switch responseData.result {
+            case .success(let value):
+                let swiftyJsonVar = JSON(value)
+                
+                if let resData = swiftyJsonVar[cat1].arrayObject {
+                    self.arrRes = resData as! [[String: AnyObject]]
+                }
+                
+                if self.arrRes.count > 0 {
+                    self.tblView.reloadData()
+                }
+                
+            case .failure(let error):
+                print("Error fetching data: \(error.localizedDescription)")
+            }
+        }
+    }
+   
+
+    // Refresh control action
+    @objc func refresh(sender: AnyObject) {
+        getData()
+        
+        // End refresh control animation after 2 seconds
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.refreshControl.endRefreshing()
+            self.view.setNeedsDisplay()
+        }
+    }
+    
+    // Show alert if no internet connection
+    func showEventsAccessDeniedAlert() {
+        let alertController = UIAlertController(title: "No Internet Connection", message: "Please Connect Your Mobile Data or WIFI Connection", preferredStyle: .alert)
+        
+        let settingsAction = UIAlertAction(title: "Settings", style: .default) { _ in
+            if let appSettings = URL(string: UIApplication.openSettingsURLString) {
+                UIApplication.shared.open(appSettings)
+            }
+        }
+        
+        alertController.addAction(settingsAction)
+        
+        let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+}
---
+++ Live TVTests/Live_TVTests.swift
@@ -0,0 +1,17 @@
+//
+//  Live_TVTests.swift
+//  Live TVTests
+//
+//  Created by Md Jobaid Azim on 2/15/25.
+//
+
+import Testing
+@testable import Live_TV
+
+struct Live_TVTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
